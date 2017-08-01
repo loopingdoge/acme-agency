e192513ec085c23c934b25e45bea87c6d6ff1f5f
Filippo Morselli <exmorse@gmail.com>
2017-08-01 18:48:42 +0200

    Cancello vecchi esempi

--- esempi-java-jolie/JolieCallsJava/client/generateIol.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Generate Jolie interface from WSDL file
-
-# Then remove the first line of created file
-
-wsdl2jolie http://localhost:8080/jolie-calls-java-0.0.1-SNAPSHOT/HelloService?WSDL > GeneratedInterface.iol
