de372d656ac058008cf49c84fb5d5c1242da8eb7
Filippo Morselli <exmorse@gmail.com>
2017-08-14 16:08:53 +0200

    Start Camunda process from Java web service

--- src/acmeagency-ws/src/main/java/soseng/project/acme/BuyerWebService.java
+++ src/acmeagency-ws/src/main/java/soseng/project/acme/BuyerWebService.java
@@ -1,8 +1,13 @@
 package soseng.project.acme;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.jws.*;
+import javax.annotation.Resource;
+
+import org.camunda.bpm.engine.ProcessEngine;
 
 import soseng.project.utils.Address;
 import soseng.project.utils.House;
@@ -13,10 +18,12 @@ import soseng.project.utils.HouseProfile;
 @SuppressWarnings("restriction")
 @WebService
 public class BuyerWebService {
+	@Resource(mappedName = "java:global/camunda-bpm-platform/process-engine/default")
+	private ProcessEngine processEngine;
+
 	
 	public BuyerWebService () {}
 
-	
 	@WebMethod
 	public ArrayList<House> requestHouses(
 			@WebParam(name="houseProfile")
@@ -24,6 +31,7 @@ public class BuyerWebService {
 			@WebParam(name="buyerName")
 			String buyerName) {
 		
+		/* House examples */
 		ArrayList<House> houseList = new ArrayList<House>();
 		houseList.add(new House(
 				new Address("Italia", "MO", "Quarantoli", "Via Unica", "31"),
@@ -37,6 +45,17 @@ public class BuyerWebService {
 				new Address("Italia", "MO", "Medolla", "Via Lunga", "1"), 
 				"Casa sull'Albero", "Luciano", 25, true, 7000)
 		);
+		
+
+		String message = "MSG";
+		Map<String, Object> vars = new HashMap<String, Object>();
+		/* Process initial variables */
+		vars.put("name", message);
+		vars.put("emptyMessage", "");
+		
+		/* Start a process in Camunda, which is waiting on the specified messageId */
+		String startMessageId = "AcmeOfferMessage";
+		processEngine.getRuntimeService().startProcessInstanceByMessage(startMessageId, vars);
 		
 		return houseList;
 	}
