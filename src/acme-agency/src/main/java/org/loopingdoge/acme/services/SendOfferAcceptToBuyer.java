37686e26855ba2771409cdd470ac8239bd2c8183
Alberto Nicoletti <albyx.n@gmail.com>
2017-08-24 19:54:50 +0200

    Offer classes created

---
+++ src/acme-agency/src/main/java/org/loopingdoge/acme/services/SendOfferAcceptToBuyer.java
@@ -0,0 +1,18 @@
+package org.loopingdoge.acme.services;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.logging.Logger;
+
+public class SendOfferAcceptToBuyer implements JavaDelegate {
+
+    private final static Logger logger = Logger.getLogger("SendOfferAcceptToBuyer");
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        logger.info("service started");
+        // TODO implement this
+    }
+
+}
