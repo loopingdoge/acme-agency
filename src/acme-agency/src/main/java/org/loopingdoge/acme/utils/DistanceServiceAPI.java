cc95a147c8947d26706a4b697cf45f636e84f0b3
Alberto Nicoletti <albyx.n@gmail.com>
2017-08-25 15:09:15 +0200

    Added mail service Java API

--- src/acme-agency/src/main/java/org/loopingdoge/acme/utils/DistanceServiceAPI.java
+++ src/acme-agency/src/main/java/org/loopingdoge/acme/utils/DistanceServiceAPI.java
@@ -2,7 +2,6 @@ package org.loopingdoge.acme.utils;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface DistanceServiceAPI {
---
+++ src/acme-agency/src/main/java/org/loopingdoge/acme/utils/MailServiceAPI.java
@@ -0,0 +1,12 @@
+package org.loopingdoge.acme.utils;
+
+import retrofit2.Call;
+import retrofit2.http.*;
+
+public interface MailServiceAPI {
+    @GET("/{user}")
+    Call<MailGetResponse> read(@Path("user") String user, @Query("unread") boolean unread);
+
+    @POST("/{user}")
+    Call<MailPostResponse> send(@Path("user") String user, @Field("from") String from, @Field("text") String text);
+}
