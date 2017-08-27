ec46d03b09a355f1466c5a255eb4fbbe7383f3bc
Filippo Morselli <exmorse@gmail.com>
2017-08-27 19:04:08 +0200

    Modified wsimport script

--- generate_ws.sh
+++ generate_ws.sh
@@ -1,1 +1,19 @@
-wsconsume -k -p org.loopingdoge.acme.jolie.cadastre -s src/acme-agency/src/main/java -o /dev/null src/cadastre/cadastre.wsdl
+#!/bin/bash
+
+# cadastre interface for acme-agency 
+wsimport -keep -p org.loopingdoge.acme.jolie.cadastre -s src/acme-agency/src/main/java -extension src/cadastre/cadastre.wsdl
+
+# session-manager interface for acme-agency 
+wsimport -keep -p org.loopingdoge.acme.jolie.sessionmanager -s src/acme-agency/src/main/java -extension src/camunda-session-manager/acme_session_manager.wsdl
+
+# acme-agency-ws interface for java-buyer
+wsimport -keep -p soseng.project.wsinterface -s src/java-buyer/src -extension http://localhost:8080/acme-agency-ws-0.0.1-SNAPSHOT/BuyerWebService?wsdl
+
+# session-manager interface for java-buyer
+wsimport -keep -p org.loopingdoge.acme.jolie.sessionmanager -s src/java-buyer/src -extension src/camunda-session-manager/client_session_manager.wsdl
+
+# acme-agency-ws interface for java-seller
+wsimport -keep -p soseng.project.wsinterface -s src/java-seller/src -extension http://localhost:8080/acme-agency-ws-0.0.1-SNAPSHOT/SellerWebService?wsdl
+
+# session-manager interface for java-seller
+wsimport -keep -p org.loopingdoge.acme.jolie.sessionmanager -s src/java-seller/src -extension src/camunda-session-manager/client_session_manager.wsdl
