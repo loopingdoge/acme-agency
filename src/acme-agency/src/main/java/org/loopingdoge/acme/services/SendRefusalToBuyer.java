c4a7c6ce11354972459b4acc3e3f227e7c054361
Alberto Nicoletti <albyx.n@gmail.com>
2017-08-27 17:00:25 +0200

    Implementend offer seding/acceptance/refusal (not tested yet)

--- src/acme-agency/src/main/java/org/loopingdoge/acme/services/SendRefusalToBuyer.java
+++ src/acme-agency/src/main/java/org/loopingdoge/acme/services/SendRefusalToBuyer.java
@@ -2,6 +2,10 @@ package org.loopingdoge.acme.services;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.loopingdoge.acme.model.House;
+import org.loopingdoge.acme.utils.MailServiceAPI;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.util.logging.Logger;
 
@@ -9,10 +13,28 @@ public class SendRefusalToBuyer implements JavaDelegate {
 
     private final static Logger logger = Logger.getLogger("SendRefusalToBuyer");
 
+    private final String BASE_URL = "http://localhost:7774/";
+
+    private Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    private MailServiceAPI mailService = retrofit.create(MailServiceAPI.class);
+
     @Override
     public void execute(DelegateExecution execution) throws Exception {
         logger.info("service started");
-        // TODO implement this
+
+        House chosenHouse = (House) execution.getVariable("chosenHouse");
+        String buyerName = (String) execution.getVariable("buyerName");
+        Double buyerOffer = (Double) execution.getVariable("buyerOffer");
+
+        mailService.send(
+                buyerName,
+                "AcmeAgency",
+                chosenHouse.getSellerName() + " has refused your " + buyerOffer + " euro offer")
+                .execute().body();
     }
 
 }
