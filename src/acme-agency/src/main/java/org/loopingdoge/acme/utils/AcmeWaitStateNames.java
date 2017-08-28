232d8828dbe7fac91017fc308181ed07a041f2b0
Filippo Morselli <exmorse@gmail.com>
2017-08-28 18:37:31 +0200

    Interactive java-buyer and java-seller (incomplete)

--- src/acme-agency/src/main/java/org/loopingdoge/acme/utils/AcmeWaitStateNames.java
+++ src/acme-agency/src/main/java/org/loopingdoge/acme/utils/AcmeWaitStateNames.java
@@ -7,5 +7,7 @@ public class AcmeWaitStateNames {
     public static final String WAIT_BUYER_MEETING_RESPONSE = "WaitingForBuyerMeetingResponse";
 
     public static final String WAIT_SELLER_MEETING_RESPONSE = "WaitingForSellerMeetingResponse";
+    
+    public static final String WAIT_FOR_BUYER_OFFER = "WaitingForBuyerOffer";
 
 }
