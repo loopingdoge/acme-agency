cfeace7b557c331ec29825637bc6598d25038f4a
Alberto Nicoletti <albyx.n@gmail.com>
2017-09-02 15:05:47 +0200

    Corrected scripts to start the bank and deleted an IntelliJ file

--- start_services.bat
+++ start_services.bat
@@ -4,11 +4,14 @@ SET ACME_ROOT=%~dp0
 cd src\cadastre
 start jolie server.ol
 
-cd %ACME_ROOT%\src\mail 
-start node mail.js
+cd %ACME_ROOT%\src\bank
+start jolie server.ol
 
 cd %ACME_ROOT%\src\camunda-session-manager
 start jolie session_manager.ol
+
+cd %ACME_ROOT%\src\mail 
+start node mail.js
 
 cd %ACME_ROOT%\src\distance
 start node distance.js -a env MAPS_API_KEY
--- start_services.sh
+++ start_services.sh
@@ -2,9 +2,11 @@ export ACME_ROOT=pwd
 
 cd $ACME_ROOT/src/cadastre && jolie server.ol
 &
-cd $ACME_ROOT/src/mail && node mail.js
-&
 cd $ACME_ROOT/src/camunda-session-manager && jolie session_manager.ol
+&
+cd $ACME_ROOT/src/bank && jolie server.ol
+&
+cd $ACME_ROOT/src/mail && node mail.js
 &
 cd $ACME_ROOT/src/distance && node distance.js -a env MAPS_API_KEY
 
