e473ad42b0fb036a633271074362dab44e06fd92
Filippo Morselli <exmorse@gmail.com>
2017-09-02 17:06:12 +0200

    Modified start_service.sh

--- scripts/start_services.sh
+++ scripts/start_services.sh
@@ -1,14 +1,10 @@
-SCRIPTDIR=$(dirname "$0")
-ACME_ROOT=SCRIPTDIR/..
+SCRIPTDIR=`dirname "$0"`
+ACME_ROOT=${SCRIPTDIR}/..
 
-cd $ACME_ROOT/src/cadastre && jolie server.ol
-&
-cd $ACME_ROOT/src/camunda-session-manager && jolie session_manager.ol
-&
-cd $ACME_ROOT/src/bank && jolie server.ol
-&
-cd $ACME_ROOT/src/mail && node mail.js
-&
-cd $ACME_ROOT/src/distance && node distance.js -a env MAPS_API_KEY
+cd $ACME_ROOT/src/cadastre && jolie server.ol &
+cd $ACME_ROOT/src/camunda-session-manager && jolie session_manager.ol &
+cd $ACME_ROOT/src/bank && jolie server.ol &
+cd $ACME_ROOT/src/mail && node mail.js &
+cd $ACME_ROOT/src/distance && node distance.js -a env MAPS_API_KEY &
 
 cd $ACME_ROOT
